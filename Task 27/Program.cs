9f0e901eea9c3b2d4b0a5f308bf3dd3e6d593b7b
Ilya <fominiv10@gmail.com>
2022-09-12 22:53:01 +0300

    solved the twenty-seventh task

---
+++ Task 27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumNumInside(int num)
+{
+    num = Math.Abs(num);
+    int arg1 = 0;
+    int result = 0;
+    while(num > 0)
+    {
+        arg1 = num % 10;
+        num = num / 10;
+        result = result + arg1;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число : ");
+int number = int.Parse(Console.ReadLine());
+int result = SumNumInside(number);
+Console.WriteLine($"Сумма чисел внутри числа {number} равна {result}");
